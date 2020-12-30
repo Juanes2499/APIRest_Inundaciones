d0539dcdc9a6643f02f727564b79bd374895c2fd
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2020-12-29 21:32:50 -0500

    Se termina los endpoints con autenticación para la configuración de variables nodo sensor

--- api_rest/configuracionVariablesNodoSensor/configuracionVariablesNodoSensor.controller.js
+++ api_rest/configuracionVariablesNodoSensor/configuracionVariablesNodoSensor.controller.js
@@ -1,5 +1,9 @@
 const {
     crear_configuracioVariableNodoSensor,
+    consultar_configuracionesVariablesNodoSensor,
+    consultar_configuracionVariablesNodoSensor_ByNombreVariable,
+    consultar_configuracionVariablesNodoSensor_ByIDConfiguracion_ByIDNodoSensor_ByIDVariable,
+    eliminar_configuracionVariablesNodoSensor_ByIDConfiguracion,
 } = require('./configuracionVariablesNodoSensor.service');
 
 module.exports = {
@@ -21,6 +25,85 @@ module.exports = {
                 success:state,
                 statusCode:201,
                 message: `The variable configuration with ID_NODO_SENSOR: ${body.id_nodo_sensor} and ID_VARIABLE: ${body.id_variable} was successfully created`
+            })
+        })
+    },
+    consultarConfiguracionesVariablesNodoSensor: (req, res) => {
+        consultar_configuracionesVariablesNodoSensor((err, result, state) => {
+            if(err){
+                console.log(err);
+                return res.status(403).json({
+                    success:state,
+                    statusCode:403,
+                    message: "Database get error - error in consultarConfiguracionesVariablesNodoSensor"
+                });
+            }
+            return res.status(200).json({
+                success: state,
+                statusCode: 200,
+                data:result
+            })
+        })
+    },
+    consultarConfiguracionVariablesNodoSensorByNombreVariable: (req, res) => {
+        
+        const body = req.body;
+
+        consultar_configuracionVariablesNodoSensor_ByNombreVariable(body, (err, result, state) => {
+            if(state === false){
+                return res.status(403).json({
+                    success: state, 
+                    statusCode: 403,
+                    message: "Database get error - error in consultarConfiguracionVariablesNodoSensorByNombreVariable",
+                    return: err
+                });
+            }else if(result.length > 0){
+                return res.status(200).json({
+                    success: state,
+                    statusCode: 200,
+                    data:result
+                })
+            }
+        })
+    },
+    consultarConfiguracionVariablesNodoSensorByIDConfiguracionByIDNodoSensorByIDVariable: (req, res) => {
+
+        const body = req.body;
+
+        consultar_configuracionVariablesNodoSensor_ByIDConfiguracion_ByIDNodoSensor_ByIDVariable(body, (err, result, state) => {
+            if(state === false){
+                return res.status(403).json({
+                    success: state, 
+                    statusCode: 403,
+                    message: "Database get error - error in consultarConfiguracionVariablesNodoSensorByIDConfiguracionByIDNodoSensorByIDVariable",
+                    return: err
+                });
+            }else if(result.length > 0){
+                return res.status(200).json({
+                    success: state,
+                    statusCode: 200,
+                    data:result
+                })
+            }
+        })
+    },
+    eliminarConfiguracionVariablesNodoSensorByIDConfiguracion: (req, res) => {
+
+        const body = req.body;
+
+        eliminar_configuracionVariablesNodoSensor_ByIDConfiguracion(body, (err, result, state) => {
+            if(state === false){
+                return res.status(403).json({
+                    success:state,
+                    statusCode:403,
+                    message: "Database delete error - error in eliminarConfiguracionVariablesNodoSensorByIDConfiguracion",
+                    return: err
+                });
+            }
+            return res.status(200).json({
+                success: state,
+                statusCode: 200,
+                message: `The variable configuration with ID_CONFIGURACION: ${body.id_configuracion} was successfully deleted`
             })
         })
     }
--- api_rest/configuracionVariablesNodoSensor/configuracionVariablesNodoSensor.service.js
+++ api_rest/configuracionVariablesNodoSensor/configuracionVariablesNodoSensor.service.js
@@ -61,12 +61,257 @@ module.exports = {
                                     }
                                 )
                             }
-
                         }
                     )
                 }
             }
         )
     },
+    consultar_configuracionesVariablesNodoSensor: (callback) => {
+
+        const queryConsultarConfiguracionVariablesNodoSensor = `
+            SELECT
+                CVNS.ID_CONFIGURACION,
+                NS.ID_NODO_SENSOR,
+                NS.LATITUD,
+                NS.LONGITUD,
+                CVNS.ID_VARIABLE,
+                (SELECT 
+                    VNS.NOMBRE_VARIABLE 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) NOMBRE_VARIABLE,
+                (SELECT 
+                    VNS.DETALLES 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) DETALLES_VARIABLE,
+                (SELECT 
+                    VNS.TIPO_DATO 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) TIPO_DATO_VARIABLE,
+                (SELECT 
+                    VNS.UNIDAD_MEDIDA 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) UNIDAD_MEDIDA_VARIABLE,
+                (SELECT 
+                    VNS.RANGO_MIN 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) RANGO_MIN_VARIABLE,
+                (SELECT 
+                    VNS.RANGO_MAX 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) RANGO_MAX_VARIABLE,
+                (SELECT 
+                    VNS.FECHA_CREACION 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) FECHA_CREACION_VARIABLE,
+                (SELECT 
+                    VNS.HORA_CREACION 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) HORA_CREACION_VARIABLE,
+                CVNS.FECHA_CREACION FECHA_CREACION_CONFIGURACION,
+                CVNS.HORA_CREACION HORA_CREACION_CONFIGURACION
+            FROM NODO_SENSOR NS
+            INNER JOIN 
+                CONFIGURACION_VARIABLES_NODO_SENSOR CVNS 
+                ON NS.ID_NODO_SENSOR = CVNS.ID_NODO_SENSOR
+        `;
+        
+        pool.query(
+            queryConsultarConfiguracionVariablesNodoSensor,
+            [],
+            (error, result) => {
+               if(error){
+                return callback(error, null, false);
+               }
+               return callback(null, result, true);
+            }
+        )
+    },
+    consultar_configuracionVariablesNodoSensor_ByNombreVariable: (data, callback) => {
+
+        data.nombre_variable = data.nombre_variable.toUpperCase();
+
+        const queryConsultarConfiguracionVariableByNombreVariable = `
+            SELECT
+                CVNS.ID_CONFIGURACION,
+                NS.ID_NODO_SENSOR,
+                NS.LATITUD,
+                NS.LONGITUD,
+                CVNS.ID_VARIABLE,
+                (SELECT 
+                    VNS.NOMBRE_VARIABLE 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) NOMBRE_VARIABLE,
+                (SELECT 
+                    VNS.DETALLES 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) DETALLES_VARIABLE,
+                (SELECT 
+                    VNS.TIPO_DATO 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) TIPO_DATO_VARIABLE,
+                (SELECT 
+                    VNS.UNIDAD_MEDIDA 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) UNIDAD_MEDIDA_VARIABLE,
+                (SELECT 
+                    VNS.RANGO_MIN 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) RANGO_MIN_VARIABLE,
+                (SELECT 
+                    VNS.RANGO_MAX 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) RANGO_MAX_VARIABLE,
+                (SELECT 
+                    VNS.FECHA_CREACION 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) FECHA_CREACION_VARIABLE,
+                (SELECT 
+                    VNS.HORA_CREACION 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) HORA_CREACION_VARIABLE,
+                CVNS.FECHA_CREACION FECHA_CREACION_CONFIGURACION,
+                CVNS.HORA_CREACION HORA_CREACION_CONFIGURACION
+            FROM NODO_SENSOR NS
+            INNER JOIN 
+                CONFIGURACION_VARIABLES_NODO_SENSOR CVNS 
+                ON NS.ID_NODO_SENSOR = CVNS.ID_NODO_SENSOR 
+            WHERE CVNS.ID_VARIABLE = (SELECT ID_VARIABLE FROM VARIABLES_NODO_SENSOR WHERE NOMBRE_VARIABLE = ?)    
+        `;
+
+        pool.query(
+            queryConsultarConfiguracionVariableByNombreVariable,
+            [data.nombre_variable],
+            (error, result) => {
+                if(result.length === 0){
+                    return callback(`The registers with NOMBRE_VARIABLE: ${data.nombre_variable} were not found`, null, false);
+                }else if(result.length > 0){
+                    return callback(null, result, true);
+                }
+            }
+        )
+    },
+    consultar_configuracionVariablesNodoSensor_ByIDConfiguracion_ByIDNodoSensor_ByIDVariable: (data, callback) => {
+
+        const queryConsultarConfiguracionVariableByIdConfiguracion_ByIDNodoSensor_ByIDVariable = `
+            SELECT
+                CVNS.ID_CONFIGURACION,
+                NS.ID_NODO_SENSOR,
+                NS.LATITUD,
+                NS.LONGITUD,
+                CVNS.ID_VARIABLE,
+                (SELECT 
+                    VNS.NOMBRE_VARIABLE 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) NOMBRE_VARIABLE,
+                (SELECT 
+                    VNS.DETALLES 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) DETALLES_VARIABLE,
+                (SELECT 
+                    VNS.TIPO_DATO 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) TIPO_DATO_VARIABLE,
+                (SELECT 
+                    VNS.UNIDAD_MEDIDA 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) UNIDAD_MEDIDA_VARIABLE,
+                (SELECT 
+                    VNS.RANGO_MIN 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) RANGO_MIN_VARIABLE,
+                (SELECT 
+                    VNS.RANGO_MAX 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) RANGO_MAX_VARIABLE,
+                (SELECT 
+                    VNS.FECHA_CREACION 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) FECHA_CREACION_VARIABLE,
+                (SELECT 
+                    VNS.HORA_CREACION 
+                FROM VARIABLES_NODO_SENSOR VNS  
+                WHERE ID_VARIABLE = CVNS.ID_VARIABLE) HORA_CREACION_VARIABLE,
+                CVNS.FECHA_CREACION FECHA_CREACION_CONFIGURACION,
+                CVNS.HORA_CREACION HORA_CREACION_CONFIGURACION
+            FROM NODO_SENSOR NS
+            INNER JOIN 
+                CONFIGURACION_VARIABLES_NODO_SENSOR CVNS 
+                ON NS.ID_NODO_SENSOR = CVNS.ID_NODO_SENSOR 
+                WHERE CVNS.ID_CONFIGURACION = ? OR NS.ID_NODO_SENSOR = ? OR CVNS.ID_VARIABLE = ?;    
+        `;
+
+        pool.query(
+            queryConsultarConfiguracionVariableByIdConfiguracion_ByIDNodoSensor_ByIDVariable,
+            [data.id_configuracion, data.id_nodo_sensor,data.id_variable],
+            (error, result) => {
+                
+                if(result.length === 0){
+
+                    const dataArray = [0,0,0];
+                    dataArray[0] = data.id_configuracion ? data.id_configuracion : 0;
+                    dataArray[1] = data.id_nodo_sensor ? data.id_nodo_sensor : 0;
+                    dataArray[2] = data.id_variable ? dataArray.id_variable : 0;
     
+                    const idsNombres = ["id_configuracion", "id_nodo_sensor", "id_variable"];
+                    const idsValidacion = [false, false, false];
+                    idsValidacion[0] = data.id_configuracion ? true : false;
+                    idsValidacion[1] = data.id_nodo_sensor ? true : false;
+                    idsValidacion[2] = data.id_variable ? true : false;
+    
+                    const posicionIDS = []
+    
+                    for(var i = 0; i<idsValidacion.length; i++){
+                        posicionIDS[i] = idsValidacion[i] === true ? i : null;
+                    }
+                    
+                    const errorArray = ["The register with"];
+    
+                    (posicionIDS.filter(x => x != null)).forEach(x => {
+                        errorArray.push(`${idsNombres[x].toUpperCase()}: ${dataArray[x]}`);
+                    })
+    
+                    const msgError = `${errorArray.toString().replace(",", " ").replace(",", " and ")} were not found`;
+
+                    return callback(msgError, null, false);
+                }else if(result.length > 0){
+                    return callback(null, result, true);
+                }
+            }
+        )
+    },
+    eliminar_configuracionVariablesNodoSensor_ByIDConfiguracion: (data, callback) => {
+
+        const queryConsultarExistenciaConfiguracion = `
+            SELECT * FROM CONFIGURACION_VARIABLES_NODO_SENSOR
+                WHERE ID_CONFIGURACION = ?
+        `;
+
+        pool.query(
+            queryConsultarExistenciaConfiguracion,
+            [data.id_configuracion],
+            (error, result) => {
+                if(result.length === 0){
+                    return callback(`The register with ID_CONFIGURACION: ${data.id_configuracion} was not found`, null, false);
+                }else if(result.length > 0){
+
+                    const queryEliminarConfiguracionByID = `
+                        DELETE FROM CONFIGURACION_VARIABLES_NODO_SENSOR
+                            WHERE ID_CONFIGURACION = ? 
+                    `;
+
+                    pool.query(
+                        queryEliminarConfiguracionByID,
+                        [data.id_configuracion],
+                        (error, result) => {
+                            if(error){
+                                return callback(`The register with ID_CONFIGURACION: ${data.id_configuracion} could not be deleted`, null, false);
+                            }
+                            return callback(null, null, true);
+                        }
+                    )
+                }
+            }
+        )
+    }
 }
