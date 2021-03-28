c936fb5875d7c524e6ef0c208981af4f16c63c25
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-03-27 20:14:55 -0500

    Modificación adicionales configuración variables

--- api_rest/configuracionVariablesNodoSensor/configuracionVariablesNodoSensor.router.js
+++ api_rest/configuracionVariablesNodoSensor/configuracionVariablesNodoSensor.router.js
@@ -4,10 +4,17 @@ const {
     eliminarConfiguracionVariablesNodoSensorByIDConfiguracion,
 } = require('./configuracionVariablesNodoSensor.controller');
 
+const {
+    consultarVariablesNodoSensorDinamico
+} = require('../variablesNodoSensor/variablesNodoSensor.controller');
+
 const router = require('express').Router();
 
 router.post("/", crearConfiguracioVariableNodoSensor);
 router.post("/get", consultarConfiguracionesVariablesNodoSensorDinamico);
 router.post("/delete", eliminarConfiguracionVariablesNodoSensorByIDConfiguracion)
+
+//Adicionales
+router.post('/variablesNodoSensor/get', consultarVariablesNodoSensorDinamico);
 
 module.exports = router;
--- api_rest/reglasNodoSensor/reglasNodoSensor.router.js
+++ api_rest/reglasNodoSensor/reglasNodoSensor.router.js
@@ -5,11 +5,18 @@ const {
     eliminarReglaNodoSensorById,
 } = require ('./reglasNodoSensor.controller');
 
+const {
+    consultarVariablesNodoSensorDinamico
+} = require('../variablesNodoSensor/variablesNodoSensor.controller');
+
 const router = require('express').Router();
 
 router.post("/", crearReglaNodoSensor);
 router.post("/get", consultarReglasNodoSensorDinamico);
 router.put("/", actualizarReglaNodoSensorById);
 router.post("/delete",eliminarReglaNodoSensorById);
+
+//Adicionales
+router.post('/variablesNodoSensor/get', consultarVariablesNodoSensorDinamico);
 
 module.exports = router;
